dc3437712e585fa279ad724618f923e7d1e74392
stupravin@gmail.com <stupravin@gmail.com>
2024-01-12 11:17:12 +0530

    updated url.py

--- project/urls.py
+++ project/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path("", include("portfolio.urls")),
     path("auth/", include("authapp.urls")),
     
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
